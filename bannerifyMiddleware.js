e415a4d81a1c625fcb024b36d08be2dace53f232
Dmitry Kovalevich <53308771+KovalevichD@users.noreply.github.com>
2022-04-28 15:40:32 +0300

    Add files via upload

---
+++ bannerifyMiddleware.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', () => {
+    const styles = document.createElement('link')
+    const grid = document.querySelector('.grid')
+    const iframes = document.querySelectorAll('iframe')
+    const source = window.location.href.replace('/index.html', '')
+
+    styles.rel = 'stylesheet'
+    document.head.appendChild(styles)
+    styles.href = 'https://kovalevichd.github.io/delve_util/bannerifyMiddleware.css'
+
+    grid.insertAdjacentHTML('afterbegin', `<a download="ads" class="download-all" href="${source}/_zips/all.zip">Download ads</a>`)
+    grid.insertAdjacentHTML('afterbegin', `
+            <a download="backups" class="download-all" href="${source}/_backups/all.zip">Download backups</a>`
+    )
+
+    iframes.forEach(i => {
+        const zip = source + '/_zips/' + i.src.replace(/(^.*\/banners\/(.*)\/.*$)/, '$2') + '.zip'
+
+        i.parentElement.insertAdjacentHTML('beforeend',
+            `<a download class="download" href="${zip}">Download</a>`
+        )
+    })
+})
